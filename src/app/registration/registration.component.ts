c8b4e491d544ddb68b117f46aa37305682b50437
Charumathi5 <charusegarane@gmail.com>
2021-09-08 17:24:06 +0530

    feat(register): updated employee type

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -13,11 +13,16 @@ export class RegistrationComponent implements OnInit {
  alreadyexists = false;
  errors = false;
   lr: any;
-  constructor(private fb: FormBuilder) { }
+  storage :any;
+  sub = false;
+  
+  
+  constructor(private fb: FormBuilder,private reg: formSubmitService) { }
   registrationForm = this.fb.group(
     {
       fullname: ['', [Validators.required,Validators.minLength(6)]],
       email: ['', Validators.required],
+      employeeType:['',Validators.required],
       phonenumber: ['',[ Validators.required, Validators.maxLength(10),Validators.minLength(10)]],
       question1: ['', Validators.required],
       answer1: ['', Validators.required],
@@ -34,8 +39,9 @@ export class RegistrationComponent implements OnInit {
   onsubmit(){
     console.log("hello");
       let formData = {
-      fullName: this.registrationForm.get('fullname').value,
+      name: this.registrationForm.get('fullname').value,
       email: this.registrationForm.get('email').value,
+      employeeType: this.registrationForm.get('employeeType').value,
       phoneNumber: this.registrationForm.get('phonenumber').value,
       question1: this.registrationForm.get('question1').value,
       answer1: this.registrationForm.get('answer1').value,
@@ -46,22 +52,22 @@ export class RegistrationComponent implements OnInit {
      
     };
     
-    // this.lr.onFormSubmit(formData).subscribe(
-    //   (response) => {
-    //     console.log(response);
-    //     if (response.message === 'User Registered Successfully') {
-    //       this.submitted = true;
-    //     } else {
-    //       this.alreadyexists = true;
-    //     }
-    //   },
-    //   (err) => {
-    //     this.errors = true;
-    //   }
-    // );
+    this.reg.onFormSubmit(formData).subscribe(
+      (response) => {
+        console.log(response);
+        if (response.status === 'ADDED' ) {
+          this.storage = response.message;
+          this.submitted = true;
+          this.sub = true;
+        } else if(response.message === 'User already present'){
+          this.alreadyexists = true;
+        }
+      },
+      (err) => {
+        this.errors = true;
+      }
+    );
   }
   
 ngOnInit(): void{}
   }
-  
-
